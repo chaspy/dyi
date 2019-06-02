0ea2f1255161fe3f51f13d78a495c5c6a8380e2a
kondo takeshi <chaspy@users.noreply.github.com>
2019-06-03 00:27:13 +0900

    Associate user from EventUser

--- db/migrate/20190602152324_create_event_users.rb
+++ db/migrate/20190602152324_create_event_users.rb
@@ -2,7 +2,7 @@ class CreateEventUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :event_users do |t|
       t.references :event, null: false, foreign_key: true
-      t.referebces :user
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
