2899823fb34f741d96b8364cb42685050bd54af8
kondo takeshi <chaspy@users.noreply.github.com>
2019-06-03 01:37:03 +0900

    Add attributes for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, :confirmable, omniauth_providers: %i(github)
   has_many :event_user, dependent: :destroy
   has_many :event, through: :event_user
   has_many :group_user, dependent: :destroy
