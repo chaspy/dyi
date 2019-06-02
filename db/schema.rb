3d6894481eec4c15c65bb6c77817a2873e35251f
kondo takeshi <chaspy@users.noreply.github.com>
2019-06-03 02:09:13 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_162924) do
+ActiveRecord::Schema.define(version: 2019_06_02_170000) do
 
   create_table "event_users", force: :cascade do |t|
     t.integer "event_id", null: false
@@ -57,8 +57,12 @@ ActiveRecord::Schema.define(version: 2019_06_02_162924) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "provider", default: "", null: false
+    t.string "uid", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "event_users", "events"
