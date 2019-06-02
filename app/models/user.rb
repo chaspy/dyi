62220e0b92443b7517df8e821af50df95358ee82
kondo takeshi <chaspy@users.noreply.github.com>
2019-06-03 01:31:18 +0900

    rails g devise user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :event_user, dependent: :destroy
   has_many :event, through: :event_user
   has_many :group_user, dependent: :destroy
