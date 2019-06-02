5f23879731e86da88ebf97ed67697d5c2f17009a
kondo takeshi <chaspy@users.noreply.github.com>
2019-06-03 01:53:00 +0900

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'oauth_test/index'
   devise_for :users, controllers: {
-    registrations: "users/registrations"
+    registrations: "users/registrations",
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
   resources :groups
   resources :users
