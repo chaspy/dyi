103d8614db7a345e04339bf46a18a1888b0045e9
kondo takeshi <chaspy@users.noreply.github.com>
2019-06-03 01:05:27 +0900

    Associate user from GroupUser

--- db/migrate/20190602160214_create_group_users.rb
+++ db/migrate/20190602160214_create_group_users.rb
@@ -2,7 +2,7 @@ class CreateGroupUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :group_users do |t|
       t.references :group, null: false, foreign_key: true
-      t.references :user
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
